b0fb4f9be227b316acbf9ff719dc1eea19e6593a
alluriprasad <alluriprasad@HDDLBGKIOSK13.Virtusa.com>
2019-06-11 15:14:15 +0530

    commited

---
+++ src/test/java/Code/Add.java
@@ -0,0 +1,13 @@
+package Code;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   int a=10, b=20;
+    int c=a+b;
+    System.out.println("The value of c is:"+c);
+	}
+
+}
+
